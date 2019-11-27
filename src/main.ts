d5b598507762df21619a060467df9ea07bc0126c
arindamdawn <arindamdawn3@gmail.com>
2019-11-27 13:59:52 +0530

    Added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); //global validation pipe
+
+  //configure swagger
+  const options = new DocumentBuilder()
+    .setTitle('Nest Js Practice')
+    .setDescription('Nest JS practice with MySQL database')
+    .setVersion('1.0')
+    .addTag('Products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
